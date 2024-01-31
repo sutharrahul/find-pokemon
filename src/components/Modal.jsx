d829d90abec511c12ce335d2a64c714cbb333fe4
Rahul suthar <sutharrahul119@gmail.com>
2024-01-31 22:21:41 +0530

    modal colse handel :tada:

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const Modal = ({ onClose, children }) => {
   return (
     <div className={`fixed inset-0 z-50 flex items-center justify-center`}>
-      <div className="fixed inset-0 bg-black opacity-50"></div>
+      <div
+        onClick={onClose}
+        className="fixed inset-0 bg-black opacity-50"
+      ></div>
 
       <div className="bg-white p-6 rounded-lg z-10">
         <div className="flex justify-between ">

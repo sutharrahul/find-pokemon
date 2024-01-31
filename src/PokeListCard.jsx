0d566f3c057b9f38383fbfd16c932d369140c82b
Rahul suthar <sutharrahul119@gmail.com>
2024-01-31 22:13:56 +0530

    :tada: :fire:

--- src/PokeListCard.jsx
+++ src/PokeListCard.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { fetchPokemonData } from "../service";
 import LoadingEffect from "./LoadingEffect";
 

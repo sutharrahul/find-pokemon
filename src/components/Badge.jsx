635cebc23f960a90dcb99f0c1a2fdb05140dad88
Rahul suthar <sutharrahul119@gmail.com>
2024-01-31 22:16:51 +0530

    fevicon add

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Badge({ text }) {
   return (
-    <span class="px-2 py-[1px] ml-[2px] my-[2px] text-xs capitalize rounded-full text-red-600 border border-red-600 bg-red-200 ">
+    <span className="px-2 py-[1px] ml-[2px] my-[2px] text-xs capitalize rounded-full text-red-600 border border-red-600 bg-red-200 ">
       {text}
     </span>
   );
